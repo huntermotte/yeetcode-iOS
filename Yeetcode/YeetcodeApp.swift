20a6fb2b9956ea07c048ef6a8908d2f4690b9427
huntermotte <hunter@harvestedfinancial.com>
2024-09-18 21:56:49 -0400

    Initial Commit

---
+++ Yeetcode/YeetcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeetcodeApp.swift
+//  Yeetcode
+//
+//  Created by Hunter Motte on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct YeetcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YeetcodeTests/YeetcodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YeetcodeTests.swift
+//  YeetcodeTests
+//
+//  Created by Hunter Motte on 9/18/24.
+//
+
+import Testing
+@testable import Yeetcode
+
+struct YeetcodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
